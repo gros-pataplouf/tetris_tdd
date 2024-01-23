588eb3f91b3f82a343d6775eeb59616b00baf51b
gros-pataplouf <sceres@pm.me>
2024-01-23 21:12:02 +0100

    can be rotated right/clockwise

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -11,4 +11,14 @@ export class RotatingShape {
     toString(){
         return this.#matrixToString(this.shape)
     }
+    rotateRight() {
+        const copyOfShape = this.shape.map(elt => elt.map(x => x))
+        for (let i = 0; i < this.shape.length; i ++) {
+            for (let j = 0; j < this.shape.length; j++) {
+                copyOfShape[j][this.shape.length - 1 - i] = this.shape[i][j]
+            }
+        }
+        const stringRepOfRotatedShape = this.#matrixToString(copyOfShape)
+        return new RotatingShape(stringRepOfRotatedShape)
+    }
 }
