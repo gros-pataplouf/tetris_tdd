06b01d24e4b717089a0feabe762b63fe8bcb1dc0
gros-pataplouf <sceres@pm.me>
2024-01-23 21:09:37 +0100

    refactor

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -3,9 +3,12 @@ export class RotatingShape {
         this.shape = shape.split('\n').map(elt => elt.trim().split(''))
         Object.freeze(this)
     }
-    toString(){
-        const joinedSubarrays =  this.shape.map(elt => elt.join('')).map(x => x + '\n')
+    #matrixToString(matrix) {
+        const joinedSubarrays =  matrix.map(elt => elt.join('')).map(x => x + '\n')
         const reconstitutedShapeString = joinedSubarrays.reduce((acc, subarr) => acc.concat(subarr))
-        return reconstitutedShapeString
-    }        
+        return reconstitutedShapeString.replace('\n\n','\n')
+    }
+    toString(){
+        return this.#matrixToString(this.shape)
+    }
 }
