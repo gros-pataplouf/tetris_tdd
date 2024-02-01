d1a9f7da8a984a45f66d7a52b7e1ab8e1a34d088
gros-pataplouf <sceres@pm.me>
2024-02-01 22:25:23 +0100

    tests pass

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,7 +4,14 @@ const ComplexMixin = Sup => class extends Sup {
     constructor(shape, orientations = 4) {
         super(shape)
         this.orientations = orientations
-    }}
+    }
+    static getShapeOrientations() {
+        let shapes = []
+        for (let i = 0; i < this.orientations; i++) {
+            shapes.push(this.rotateRight())
+        }
+    }
+}
 
 class Tetro extends ComplexMixin(RotatingShape) {}
 
