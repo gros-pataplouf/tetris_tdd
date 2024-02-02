255405408640ed4171c79924d816a844fa9777b0
gros-pataplouf <sceres@pm.me>
2024-02-02 18:13:49 +0100

    tests pass

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -10,14 +10,14 @@ const RotatableComplex = Sup => class extends Sup {
             for (let i = 0; i < index; i++) {
                 rotationResult = rotateMatrix(rotationResult)                  
             }
-            return this.toString(rotationResult)
+            return this.matrixToString(rotationResult)
         })
     }
     rotateRight() {
-        console.log(this.variants)
-        const rotatedMatrix = rotateMatrix(this.shape)
-        const stringRepr = this.matrixToString(rotatedMatrix)
-        return new RotatingTetromino(stringRepr)
+        const currentStringRepr = this.toString()
+        const currentRotationIndex = this.variants.indexOf(currentStringRepr)
+        const rotatedStringRepr = this.variants[(currentRotationIndex + 1) % this.variants.length ]
+        return new RotatingTetromino(rotatedStringRepr)
     }
     rotateLeft() {
         return this.rotateRight().rotateRight().rotateRight()
