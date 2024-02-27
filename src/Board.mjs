0259acba3585431ae295ce6b8cb4e7efea60f627
gros-pataplouf <sceres@pm.me>
2024-02-27 16:57:59 +0100

    tests pass

--- src/Board.mjs
+++ src/Board.mjs
@@ -53,16 +53,12 @@ export class Board {
   currentCellCanMove(rowIndex, colIndex, dirX, dirY) {
     const {block, x, y} = this.fallingShape;
     const cellIsFull = () => block.shape[rowIndex][colIndex] !== '.'
-    const isBorderCell = () => !block.shape[rowIndex + dirY] || !block.shape[rowIndex + dirY][colIndex + dirX] || block.shape[rowIndex + dirY][colIndex + dirX] === '.'
+    const isBorderCell = (dirX, dirY) => !block.shape[rowIndex + dirY] || !block.shape[rowIndex + dirY][colIndex + dirX] || block.shape[rowIndex + dirY][colIndex + dirX] === '.'
     const canMoveInDirection = (dirX, dirY) => this.board[y + rowIndex + dirY] &&  this.board[y + rowIndex + dirY][x+ colIndex + dirX] === '.'
-   
-    if (cellIsFull() && isBorderCell()) {
-      return canMoveInDirection(dirX, dirY)
-    } else {
-      return !(cellIsFull()
-      && isBorderCell()
-      && !canMoveInDirection(dirX, dirY))
+    if (cellIsFull() && isBorderCell(dirX, dirY)) {
+      return canMoveInDirection(dirX, dirY) //check for full border cells whether there space on the board
     }
+    return true
   }
   moveCellOnNextBoard(rowIndex, colIndex, dirX, dirY) {
     const {block, x, y} = this.fallingShape;
