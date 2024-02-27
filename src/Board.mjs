a124d7857cca46c946cc351142035b07bfd70d60
gros-pataplouf <sceres@pm.me>
2024-02-27 18:36:16 +0100

    tests pass

--- src/Board.mjs
+++ src/Board.mjs
@@ -74,7 +74,6 @@ export class Board {
     }
     const {block, x, y} = this.fallingShape;
     this.nextBoard = this.board.map(row => row.map(elt => elt)) // work on a copy while looking at next board, so current board can be overwritten without further looping
-
     for (let rowIndex = block.height - 1; rowIndex >= 0; rowIndex--) {
       for (let colIndex = dirX <= 0? 0: block.width -1; dirX <= 0? colIndex < block.width : colIndex >= 0; dirX <= 0? colIndex++ : colIndex--) { //whether we loop from left to right or right 
         if (this.currentCellCanMove(rowIndex, colIndex, dirX, dirY)) {
