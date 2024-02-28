22b12728df895fcf38be6403fae644c3206843d2
gros-pataplouf <sceres@pm.me>
2024-02-28 17:02:33 +0100

    tests pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,9 +1,8 @@
 import { Shape } from "./Shape.mjs"
-import { rotateMatrix } from "./helpers.mjs"
 
 const RotatableSimple = Sup => class extends Sup {
     rotateRight() {
-        const rotatedMatrix = rotateMatrix(this.shape)
+        const rotatedMatrix = this.rotateMatrix(this.shape)
         const stringRepr = this.matrixToString(rotatedMatrix)
         return new RotatingShape(stringRepr)
     }
