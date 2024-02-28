c12473b41fa32a204a9a3306718c37ffd4a44649
gros-pataplouf <sceres@pm.me>
2024-02-28 17:05:14 +0100

    tests pass

--- src/helpers.mjs
+++ src/helpers.mjs
@@ -1,12 +1,4 @@
-export function rotateMatrix(sourceMatrix) {
-    const targetMatrix = sourceMatrix.map(elt => elt.map(x => x))
-    for (let i = 0; i < sourceMatrix.length; i ++) {
-        for (let j = 0; j < sourceMatrix.length; j++) {
-            targetMatrix[j][sourceMatrix.length - 1 - i] = sourceMatrix[i][j]
-        }
-    }
-    return targetMatrix
-}
+
 
 
 export function loopAndExecute(height, width, dirX, fn) {
