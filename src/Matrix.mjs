8791628b2b7b5a7ae47ba0b52147fe351ae29341
gros-pataplouf <sceres@pm.me>
2024-02-28 17:01:02 +0100

    tests pass

--- src/Matrix.mjs
+++ src/Matrix.mjs
@@ -5,10 +5,10 @@ export default class Matrix {
         return reconstitutedShapeString
     }
     rotateMatrix(input=this) {
-        const targetMatrix = this.map(elt => elt.map(x => x))
-        for (let i = 0; i < this.length; i ++) {
-            for (let j = 0; j < this.length; j++) {
-                targetMatrix[j][this.length - 1 - i] = this[i][j]
+        const targetMatrix = input.map(elt => elt.map(x => x))
+        for (let i = 0; i < input.length; i ++) {
+            for (let j = 0; j < input.length; j++) {
+                targetMatrix[j][input.length - 1 - i] = input[i][j]
             }
         }
         return targetMatrix
