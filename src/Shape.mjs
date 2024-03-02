761177575a1006ab11b6e3d0e819f52f752d1d78
gros-pataplouf <sceres@pm.me>
2024-03-02 07:55:48 +0100

    reformatting by changing the indent does not count as changing production code

--- src/Shape.mjs
+++ src/Shape.mjs
@@ -1,19 +1,19 @@
 import MatrixToolsProvider from "./Matrix.mjs"
 
 export class Shape extends MatrixToolsProvider {
-    constructor(shape) {
-        super()
-        this.shape = shape.trim().split('\n').map(elt => elt.trim().split(''))
-        this.width = this.shape[0].length
-        this.height = this.shape.length
-    }
-     toString(){
-        return this.matrixToString(this.shape)
-    }
-    nextCellInDirectionEmptyOrNull(rowIndex, colIndex, dirX, dirY) {
-      return !this.shape[rowIndex + dirY] || !this.shape[rowIndex + dirY][colIndex + dirX] || this.shape[rowIndex + dirY][colIndex + dirX] === '.';
-    }
-    currentCellIsFull(rowIndex, colIndex) {
-      return this.shape[rowIndex][colIndex] !== '.'
-    }
+  constructor(shape) {
+      super()
+      this.shape = shape.trim().split('\n').map(elt => elt.trim().split(''))
+      this.width = this.shape[0].length
+      this.height = this.shape.length
+  }
+  toString(){
+    return this.matrixToString(this.shape)
+  }
+  nextCellInDirectionEmptyOrNull(rowIndex, colIndex, dirX, dirY) {
+    return !this.shape[rowIndex + dirY] || !this.shape[rowIndex + dirY][colIndex + dirX] || this.shape[rowIndex + dirY][colIndex + dirX] === '.';
+  }
+  currentCellIsFull(rowIndex, colIndex) {
+    return this.shape[rowIndex][colIndex] !== '.'
+  }
 }
