367c887de6140eb43ca8b56709f316ca0f8abdb8
gros-pataplouf <sceres@pm.me>
2024-03-02 08:42:55 +0100

    tests pass

--- src/Matrix.mjs
+++ src/Matrix.mjs
@@ -10,7 +10,7 @@ export default class MatrixToolsProvider {
     rotateMatrix(input=this) {
         const targetMatrix = input.map(elt => elt.map(x => x))
         for (let i = 0; i < input.length; i ++) {
-            for (let j = 0; j < input.length; j++) {
+            for (let j = 0; j < input[0].length; j++) {
                 targetMatrix[j][input.length - 1 - i] = input[i][j]
             }
         }
